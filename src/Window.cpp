da9108c0bca8b3eb0ba8bb9ad22cfce8b6191565
Arden-Stanley <arden.stanley@yahoo.com>
2025-09-11 19:48:31 -0500

    Added very descriptive comments and documentation to Shader.h/.cpp and Window.h/.cpp and main.cpp and Common.h files

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,26 +1,73 @@
+/*
+ * File Description:
+ * 	-Implementation file of the "Window.h" file.
+ * 	-Implements the member functions for the LightRay::Window class.
+ * 	-For general member function documentation, check "Window.h".
+ */
+
 #include "Window.h"
 
 namespace LightRay {
 	Window::Window(const std::string &title, int width, int height) 
-	: _buffer(nullptr), _title(title), _width(width), _height(height) {
+	: _buffer(nullptr), _title(title), _width(width), _height(height) { 
+		
+		/*
+		 * Initialize GLFW systems with correct bindings for system.
+		 */	
 		glfwInit();
-		_buffer = glfwCreateWindow(_width, _height, _title.c_str(), NULL, NULL);	
+
+		/*
+		 * Create a GLFW window implementation with the parameters,
+		 * Store window in _buffer member variable.
+		 */
+		_buffer = glfwCreateWindow(_width, _height, _title.c_str(), NULL, NULL);
+							
+		/*
+		 * Make the GLFW window we just created (_buffer) the system 
+		 * context, load the OpenGL bindings with the system context.
+		 */		
 		glfwMakeContextCurrent(_buffer);
 		gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
 	}
 
 	Window::~Window() {
+		/*
+		 * Destroy the underlying GLFW window pointer (_buffer),
+		 * terminate all GLFW processes.
+		 */
 		glfwDestroyWindow(_buffer);
 		glfwTerminate();	
 	}
 
 	void Window::Update() const {
+		/*
+		 * Clears the screen to a solid color, aka the
+		 * specified background color of the window.
+		 */
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		/*
+		 * Swaps the back buffer that has been drawn to
+		 * with the front buffer which has already been
+		 * processed and cleared.
+		 */
 		glfwSwapBuffers(_buffer);
+
+		/*
+		 * Polls all system input events within the 
+		 * context of the window.
+		 */
 		glfwPollEvents();
 	}
 
 	bool Window::IsRunning() const {
+		/*
+		 * Checks if glfwPollEvents() has found a 
+		 * glfwWindowShouldClose() event with regards
+		 * to the buffer.  
+		 * If so, returns false.
+		 * If there is none, then this returns true.
+		 */
 		return !glfwWindowShouldClose(_buffer);
 	}
 }
--- src/Window.h
+++ src/Window.h
@@ -1,35 +1,85 @@
+/*
+ * File Contents:
+ * 	-LightRay::Window
+ * File Description:
+ * 	-This file contains the Window class under the namespace LightRay which acts as a wrapper class
+ *  	 around GLFW windows.  As such, the LightRay::Window abstraction can be used to more
+ *  	 effectively work with data in an encapsulated and easier to deal with way.
+ */
+
 #ifndef WINDOW_H
 #define WINDOW_H
 
 #include "Common.h"
 
 namespace LightRay {
+
+	/*
+	 * Description:
+	 * 	-A class which acts as a 'wrapper' around the GLFW procedural implementation
+	 * 	 of a system agnostic window.
+	 * 	-Allows for ease of utilizing and modifying window functionality within 
+	 * 	 the scope of LightRay.
+	 */
 	class Window {
 		public:
 			/*
-			 * Creates a window object with the specified window title, width, and height.
+			 * Description:
+			 * 	-Handle GLFW and OpenGL (GLAD) initialization,
+			 * 	 creates a GLFW window with specified attributes
+			 * 	 and stores it in _buffer.
+			 * Parameters:
+			 * 	-const std::string &title:
+			 * 		-The desired title (as a string or literal) that
+			 * 		 is to be displayed on window.	 
+			 *	-int width:
+			 *		-The desired width in pixels of the window.
+			 *	-int height:
+			 *		-The desired height in pixels of the window.
 			 */ 
 			Window(const std::string &title, int width, int height);
 		
 			/*
-			 * Do some shutdown work, terminate GLFW services and destroy window.
+			 * Description:
+			 * 	-Do some shutdown work, terminate GLFW services and destroy GLFW window object (_buffer).
 			 */	
 			~Window();
 		
 			/*
-			 * Performs frame-by-frame updates to the window by clearing the screen,
-			 * swapping buffers, and polling for window events.
+			 * Description:
+			 * 	-Performs frame-by-frame updates to the window by clearing the screen,
+			 * 	 swapping buffers, and polling for window events.
 			 */	
 			void Update() const;
 			
 			/*
-			 * Returns a boolean value in which true means the window is still running and
-			 * has not triggered a glfwWindowShouldClose event on polling.
+			 * Description:
+			 * 	-Allows for user to determine if the window
+			 * 	 is still active and running.
+			 * Return:
+			 * 	-A boolean in which true indicates that the window
+			 * 	 is still in fact running, and false indicates that 
+			 * 	 the window has had a close event triggered.
 			 */
 			bool IsRunning() const;
 		private:
+			/*
+			 * Description:
+			 * 	-A pointer to the underlying GLFW implementation of a window, which
+			 * 	 this LightRay::Window class is wrapped around.
+			 */
 			GLFWwindow *_buffer;
+
+			/*
+			 * Description:
+			 * 	-A string which keeps track of the title that is displayed on the window.
+			 */
 			std::string _title;
+
+			/*
+			 * Description:
+			 * 	-The width and the height in pixels of the window.
+			 */
 			int _width, _height;
 	};
 }

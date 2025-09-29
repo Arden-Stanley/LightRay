690a89d3d37e2abc41552afc030ae18d2a8f928e
Blanca Negrete <negrete.blanca19@gmail.com>
2025-09-28 20:00:38 -0500

    Working on GUI and editor display with ImGui

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,12 @@
 #include "Buffer.h"
 #include "Common.h"
 
+//ImGui Headers
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+
 int main(int argc, char** argv) 
 {
 	std::unique_ptr<LR::Window> window = std::make_unique<LR::Window>(800, 800, "Test");	
@@ -21,10 +27,34 @@ int main(int argc, char** argv)
 
 	std::unique_ptr<LR::Buffer> screenBuffer = std::make_unique<LR::Buffer>(window);
 
+	//ImGui Initialization
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(window->GetGLFWwindow(), true);
+	ImGui_ImplOpenGL3_Init("#version 330");
+
+	static float lightIntensity = 1.0f;
+	static bool showDebug = true;
+
 	while(window->IsRunning())
 	{
 		screenBuffer->Render(rtShader, bufferShader);
 		window->Update();
+
+		//ImGui Frame Start
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::Begin("Light-Ray Editor");
+		//ADD TO SCENE LOGIC HERE
+		if (ImGui::Button("Add Sphere")) {
+			// TODO: Hook this into your scene logic
+		}
+
+
 	}
 
 	return 0;

88b02f85af291c4a507291824a3e5d0855e0d9f2
Arden-Stanley <arden.stanley@yahoo.com>
2025-09-30 09:25:03 -0500

    Working on normal calculation

--- src/shaders/raytracer.glsl
+++ src/shaders/raytracer.glsl
@@ -33,17 +33,51 @@ vec4 check_hit_sphere(Sphere sphere, Ray ray)
 {
     vec3 difference = sphere.center - ray.origin;
     float a = dot(ray.dir, ray.dir);
-    float b = -2.0 * dot(ray.dir, difference);
+    float b = dot(ray.dir, difference);
     float c = dot(difference, difference) - (sphere.radius * sphere.radius);
-    float discriminant = (b * b) - (4 * a * c);
-    if (discriminant >= 0)
-    {
-        return sphere.color;
-    }
-    else 
+    float discriminant = (b * b) - (a * c);
+    if (discriminant < 0)
     {
         return vec4(0.2, 0.2, 0.2, 1.0);
     }
+    float root = (b - sqrt(discriminant)) / a;
+    if (root <= 1.0 || root >= 1000000)
+    {
+        root = (b + sqrt(discriminant)) / a;
+        if (root <= 1.0 || root >= 100000) 
+        {
+            return vec4(0.2, 0.2, 0.2, 1.0);
+        }
+    }
+
+    return sphere.color;
+}
+
+vec4 check_hit_sphere_normal(Sphere sphere, Ray ray)
+{
+    vec3 difference = sphere.center - ray.origin;
+    float a = dot(ray.dir, ray.dir);
+    float b = dot(ray.dir, difference);
+    float c = dot(difference, difference) - (sphere.radius * sphere.radius);
+    float discriminant = (b * b) - (a * c);
+    if (discriminant < 0)
+    {
+        return vec4(0.2, 0.2, 0.2, 1.0);
+    }
+    float root = (b - sqrt(discriminant)) / a;
+    if (root <= 0.0 || root >= 1000000)
+    {
+        root = (b + sqrt(discriminant)) / a;
+        if (root <= 0.0 || root >= 100000) 
+        {
+            return vec4(0.2, 0.2, 0.2, 1.0);
+        }
+    }
+
+    float t = root;
+    vec3 point = root * ray.dir;
+    vec3 normal = (point - sphere.center) / sphere.radius;
+    return vec4(normal, 1.0);
 }
 
 void main() 
@@ -73,8 +107,13 @@ void main()
 
     Sphere sphere;
     sphere.color = vec4(0.0, 1.0, 1.0, 1.0);
-    sphere.center = vec3(0.0, 0.0, -5.0);
-    sphere.radius = 1.0;
+    sphere.center = vec3(0.0, 0.0, -1.0);
+    sphere.radius = 0.5;
+
+    Sphere ground;
+    ground.color = vec4(0.0, 1.0, 0.0, 1.0);
+    ground.center = vec3(0.0, 100.5, -1.0);
+    ground.radius = 100;
 
     int samples = 100;
 
@@ -93,7 +132,12 @@ void main()
         Ray sample_ray;
         sample_ray.origin = camera.center;
         sample_ray.dir = sample_pixel - sample_ray.origin;
-        pixel = pixel + check_hit_sphere(sphere, sample_ray);
+
+        pixel = pixel + check_hit_sphere(ground, sample_ray);
+
+        vec4 normal = check_hit_sphere_normal(sphere, sample_ray);
+        pixel = pixel + 0.5 * vec4(normal.x + 1, normal.y + 1, normal.z + 1, 2.0);
+        //pixel = pixel + check_hit_sphere();
     }
 
     pixel = pixel / samples;

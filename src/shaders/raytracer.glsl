9de940f14167180a9336a3bab60cd941809d716d
stanleya1293 <arden.stanley@yahoo.com>
2025-10-08 21:55:45 -0500

    making a more intuitive sphere hit algorithm

--- src/shaders/raytracer.glsl
+++ src/shaders/raytracer.glsl
@@ -16,7 +16,7 @@ struct Camera {
 
 struct Viewport {
     float width, height;
-    vec3 u, v, du, dv, upperLeft, center;
+    vec3 u, v, du, dv, upper_left, center;
 };
 
 struct Sphere {
@@ -25,29 +25,41 @@ struct Sphere {
     float radius;
 };
 
-float getRandom(vec2 seed) {
+float get_random(vec2 seed) {
     return fract(sin(dot(seed.xy, vec2(12.9898, 78.233))) * 43758.5453123);
 }
 
-Ray getSampleRay(int seed, Viewport vp, ivec2 pixelCoords) {
+Ray get_sample_ray(int seed, Viewport vp, ivec2 pixel_coords) {
     vec3 offset =  
         vec3(
-            getRandom(time * pixelCoords * seed) - 0.5, 
-            getRandom(time * pixelCoords * seed) - 0.5, 
+            get_random(time * pixel_coords * seed) - 0.5, 
+            get_random(time * pixel_coords * seed) - 0.5, 
             0.0
         );
-        vec3 samplePixel = vp.center + ((pixelCoords.x + offset.x) * vp.dv)
-                                        + ((pixelCoords.y + offset.y) * vp.dv);
+        vec3 sample_pixel = vp.center + ((pixel_coords.x + offset.x) * vp.dv)
+                                        + ((pixel_coords.y + offset.y) * vp.dv);
 
-        Ray sampleRay;
-        sampleRay.origin = vp.center;
-        sampleRay.dir = samplePixel - sampleRay.origin;
+        Ray sample_ray;
+        sample_ray.origin = vp.center;
+        sample_ray.dir = sample_pixel - sample_ray.origin;
 
-        return sampleRay;
+        return sample_ray;
 }
 
-float getSphereHit() {
-    
+//returns t value (intersection scalar)
+float get_sphere_hit(Ray ray, Sphere sphere) {
+    vec3 offset = sphere.center - ray.origin;
+    float a = dot(ray.dir, ray.dir);
+    float b = 2.0 * dot(ray.dir, offset);
+    float c = dot(offset, offset) - (sphere.radius * sphere.radius);
+    float discriminant = (b * b) - (4 * a * c);
+
+    if (discriminant < 0) {
+        return -1;
+    }
+    else if (discriminant == 0) {
+
+    }
 }
 
 void main() {
@@ -55,7 +67,7 @@ void main() {
     Camera camera;
     camera.center = vec3(0.0, 0.0, 0.0);
 
-    ivec2 pixelCoords = ivec2(gl_GlobalInvocationID.xy);
+    ivec2 pixel_coords = ivec2(gl_GlobalInvocationID.xy);
     ivec2 size = imageSize(img);
 
     Viewport vp;
@@ -68,8 +80,8 @@ void main() {
 
     vp.du = vp.u / size.x;
     vp.dv = vp.v / size.y;
-    vp.upperLeft = camera.center - vec3(0, 0, 1.0) - vp.u/2 - vp.v/2;
-    vp.center = vp.upperLeft + 0.5 * (vp.du + vp.dv);    
+    vp.upper_left = camera.center - vec3(0, 0, 1.0) - vp.u/2 - vp.v/2;
+    vp.center = vp.upper_left + 0.5 * (vp.du + vp.dv);    
 
     Sphere sphere;
     sphere.color = vec4(0.0, 1.0, 1.0, 1.0);
@@ -83,12 +95,12 @@ void main() {
 
     int samples = 100;
 
-    vec4 pixelColor = vec4(0.0, 0.0, 0.0, 1.0);
+    vec4 pixel_color = vec4(0.0, 0.0, 0.0, 1.0);
     for (int i = 0; i < samples; i++) {
-        Ray sampleRay = getSampleRay(i * 100, vp, pixelCoords);
+        Ray sample_ray = get_sample_ray(i * 100, vp, pixel_coords);
     }
 
-    pixelColor = pixelColor / samples;
+    pixel_color = pixel_color / samples;
 
-    imageStore(img, pixelCoords, pixelColor);
+    imageStore(img, pixel_coords, pixel_color);
 }

e0a5e0b68589ca6a2d41ce9b13a2ea7bd71ba5dc
stanleya1293 <arden.stanley@yahoo.com>
2025-10-11 21:27:13 -0500

    working on normal calculations

--- src/shaders/raytracer.glsl
+++ src/shaders/raytracer.glsl
@@ -33,13 +33,13 @@ vec3 getRandomOnHemi(vec3 normal) {
     vec3 randOnSphere;
     while (true) {
         vec3 p = vec3(
-            (getRandom(gl_GlobalInvocationID.xy * 102) + 1.0) / 2.0, 
-            (getRandom(gl_GlobalInvocationID.xy * 232) + 1.0) / 2.0, 
-            (getRandom(gl_GlobalInvocationID.xy * 827) + 1.0) / 2.0
+            (getRandom(gl_GlobalInvocationID.yy * 102) * 2.0) - 1.0, 
+            (getRandom(gl_GlobalInvocationID.yy * 232) * 2.0) - 1.0, 
+            (getRandom(gl_GlobalInvocationID.yy * 827) * 2.0) - 1.0
         );
         float lengthSqrd = p.x * p.x + p.y * p.y + p.z * p.z;
         if (1e-160 < lengthSqrd && lengthSqrd <= 1)
-            randOnSphere =  p / sqrt(lengthSqrd);
+            p = normalize(p);
             break;
     }
     if (dot(randOnSphere, normal) > 0.0) {
@@ -52,28 +52,25 @@ vec3 getRandomOnHemi(vec3 normal) {
 
 //returns t value (intersection scalar)
 float getSphereHit(Ray ray, Sphere sphere) {
-    vec3 offset = ray.origin - sphere.center;
+    vec3 offset = sphere.center - ray.origin;
     float a = dot(ray.dir, ray.dir);
-    float b = 2.0 * dot(offset, ray.dir);
+    float h = dot(ray.dir, offset);
     float c = dot(offset, offset) - (sphere.radius * sphere.radius);
-    float discriminant = (b * b) - (4.0 * a * c);
+    float discriminant = (h * h) - (a * c);
 
-    if (discriminant < 0.0) {
+    if (discriminant < 0) {
         return -1;
     }
     else {
-        float t0 = (-b - sqrt(discriminant)) / (2.0 * a);
-        float t1 = (-b + sqrt(discriminant)) / (2.0 * a);
-
-        if (t0 > 0.001) {
-            return t0;
+        float sqrtd = sqrt(discriminant);
+        float root = (h - sqrtd) / a;
+        if (root <= 0.0) {
+            root = (h + sqrtd) / a;
+            if (root <= 0.0) {
+                return -1;
+            }
         }
-        else if (t1 > 0.001) {
-            return t1;
-        }
-        else {
-            return -1.0;
-        }
+        return root;
     }
 }
 
@@ -81,6 +78,7 @@ vec3 getSurfaceNormal(Ray ray, Sphere sphere, float t) {
     if (t != -1) {
         vec3 p = ray.origin + t * ray.dir;
         vec3 normal = (p - sphere.center) / sphere.radius;
+        normal = normalize(normal);
         return normal;
     }
     else {
@@ -92,8 +90,6 @@ void main() {
 
     Camera camera;
     camera.center = vec3(0.0, 0.0, 0.0);
-
-    int rayDepth = 10;
 
     ivec2 pixelCoords = ivec2(gl_GlobalInvocationID.xy);
     ivec2 size = imageSize(img);
@@ -110,10 +106,9 @@ void main() {
     vp.dv = vp.v / float(size.y);
     vp.upperLeft = camera.center - vec3(0, 0, 1.0) - vp.u/2 - vp.v/2;
     vp.firstPixel = vp.upperLeft + 0.5 * (vp.du + vp.dv);    
-
+    
     Sphere sphere = Sphere(vec4(0.0, 1.0, 1.0, 1.0), vec3(0.0, 0.0, -2.0), 1.0);
-    Sphere ground = Sphere(vec4(0.0, 1.0, 0.0, 1.0), vec3(0.0, -101.0, -2.0), 100.0);
-
+    Sphere ground = Sphere(vec4(0.0, 1.0, 0.0, 1.0), vec3(0.0, 0.0, -5.0), 3.0);
 
 
     vec3 targetPixel = vp.firstPixel + (pixelCoords.x * vp.du) + (pixelCoords.y * vp.dv);
@@ -132,21 +127,26 @@ void main() {
     Ray currentRay = ray;
     float scalar = 1;
     vec4 pixelColor;
+
+    int rayDepth = 4;
+
     for (int i = 0; i < rayDepth; i++) {
-        float tSphere = getSphereHit(currentRay, sphere);
+        //float tSphere = getSphereHit(currentRay, sphere);
         float tGround = getSphereHit(currentRay, ground);
         float t;
 
-        if (tSphere != -1 && tSphere <= tGround) {
+        /*if (tSphere != -1 && tSphere <= tGround) {
             t = tSphere;
-            scalar *= 0.5;
-        }
-        else if (tGround != -1 && tGround < tSphere) {
+            scalar *= 0.3;
+        }*/
+        
+        if (tGround != -1) {
             t = tGround;
             scalar *= 0.5;
         }
+        
         else {
-            pixelColor = vec4(0.2, 0.4, 1.0, 1.0) * scalar;
+            pixelColor = vec4(0.2, 0.8, 1.0, 1.0) * scalar;
             break;
         }
         

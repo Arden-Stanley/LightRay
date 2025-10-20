7f08068831285ac151c76cbc2bc72a148cda1560
stanleya1293 <arden.stanley@yahoo.com>
2025-10-19 21:49:57 -0500

    Working on diffuse shading again

--- src/shaders/raytracer.glsl
+++ src/shaders/raytracer.glsl
@@ -5,11 +5,15 @@ layout (rgba32f, binding = 0) uniform image2D img;
 
 uniform float time;
 
+ivec2 PIXEL_LOC = ivec2(gl_GlobalInvocationID.xy);
+ivec2 SCREEN_DIMS = imageSize(img);
+
 struct Ray {
     vec3 origin;
     vec3 direction;
     vec3 hit;
     float t;
+    vec3 normal;
 };
 
 struct Sphere {
@@ -17,49 +21,123 @@ struct Sphere {
     float radius;
 };
 
-struct Camera {
+struct Viewport {
     vec3 center;
-    float focalLength;
+    float focal_length;
+    float width;
+    float height;
+    vec3 u;
+    vec3 v;
+    vec3 du;
+    vec3 dv;
+    vec3 first_pixel;
 };
 
-bool getSphereHit(inout Ray ray, in Sphere sphere) {
-    vec3 oc = ray.origin - sphere.center;
+float get_random_f(vec2 seed) {
+    return fract(sin(dot(seed.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
+
+vec3 get_random_unit() {
+    vec3 p;
+    float length_squared;
+    float i = 10;
+    while (true) {
+        p = vec3(
+            (get_random_f(PIXEL_LOC + vec2(time + 41.0 + i, time + 102.0 + i))), 
+            (get_random_f(PIXEL_LOC + vec2(time + 67.0 + i, time + 127.0 + i))), 
+            (get_random_f(PIXEL_LOC + vec2(time + 420.0 + i, time + 273.0 + i)))
+        );
+        i += 10;
+        length_squared = p.x * p.x + p.y * p.y + p.z * p.z;
+        if (length_squared <= 1) {
+            return normalize(p);
+        }
+    };
+}
+
+vec3 get_unit_on_hemi(Ray ray) {
+    vec3 on_unit_sphere = get_random_unit();
+    if (dot(on_unit_sphere, ray.normal) > 0.0)
+        return on_unit_sphere;
+    else 
+        return -on_unit_sphere;
+}
+
+bool get_sphere_hit(inout Ray ray, in Sphere sphere) {
+    vec3 oc = sphere.center - ray.origin;
     float a = dot(ray.direction, ray.direction);
-    float s = dot(ray.direction, oc);
+    float h = dot(ray.direction, oc);
     float c = dot(oc, oc) - (sphere.radius * sphere.radius);
 
-    float delta = s * s - (c);
-    float d = -s - sqrt(delta);
+    float d = h * h - a * c;
 
-    if (delta < 0.0) {
+    if (d < 0) {
         return false;
     }
-    else {
-        return true;
+    float sqrtd = sqrt(d);
+    float root = (h - sqrtd) / a;
+    if (root <= 0.0) {
+        root = (h + sqrtd) / a;
+        if (root <= 0.0) {
+            return false;
+        }
     }
+    ray.t = d;
+    ray.hit = ray.origin + d * ray.direction;
+    ray.normal = (ray.hit - sphere.center) / sphere.radius;
+    return true;    
 }
 
 void main() {
-    ivec2 pixelLoc = ivec2(gl_GlobalInvocationID.xy);
-    ivec2 screenDims = imageSize(img);
+    Viewport viewport;
+    viewport.center = vec3(0, 0, 0);
+    viewport.focal_length = 1.0;
+    viewport.height = 2.0;
+    viewport.width = viewport.height * (float(SCREEN_DIMS.x) / SCREEN_DIMS.y);
+    viewport.u = vec3(viewport.width, 0, 0);
+    viewport.v = vec3(0, -viewport.height, 0);
+    viewport.du = viewport.u / SCREEN_DIMS.x;
+    viewport.dv = viewport.v / SCREEN_DIMS.y; 
+    viewport.first_pixel = viewport.center - vec3(0, 0, viewport.focal_length)
+         - (viewport.u / 2) - (viewport.v / 2)
+         + 0.5 * (viewport.du + viewport.dv);
 
-    vec4 pixelColor = vec4(0.2, 0.5, 0.8, 1.0);
 
-    Camera camera;
-    camera.center = vec3(0.0, 0.0, 1.0);
+    Sphere sphere1;
+    sphere1.center = vec3(0.0, 0.0, -3.0);
+    sphere1.radius = 1.0;
 
-    Sphere sphere;
-    sphere.center = vec3(0.0, 0.0, -5.0);
-    sphere.radius = 1.0;
+    Sphere sphere2;
+    sphere2.center = vec3(0.0, 100.0, -3.0);
+    sphere2.radius = 100.0;
 
+
+    vec3 target_pixel =
+        viewport.first_pixel +
+        (PIXEL_LOC.x * viewport.du) +
+        (PIXEL_LOC.y * viewport.dv);
+    
     Ray ray;
-    ray.origin = vec3(0, 0, 0);
-    ray.direction = vec3(float(pixelLoc.x), float(pixelLoc.y), 1.0) - camera.center;
+    ray.origin = viewport.center;
+    ray.direction = normalize(target_pixel - viewport.center);
 
-    if (getSphereHit(ray, sphere)) {
-        pixelColor = vec4(0.5, 0.0, 0.2, 1.0);
+    vec3 unit_direction = normalize(ray.direction);
+    float a = 0.5 * (unit_direction.y + 1.0);
+    vec3 gradient = (1.0-a) * vec3(1.0, 1.0, 1.0) + a * vec3(0.5, 0.7, 1.0);
+    vec4 pixel_color = vec4(gradient, 1.0);
+    for (int i = 0; i < 5; i++) {
+        if (get_sphere_hit(ray, sphere1)) {
+            pixel_color *= 0.9;
+        }
+        else if (get_sphere_hit(ray, sphere2)) {
+            pixel_color *= 0.8;
+        }
+        else {
+            break;
+        }
+        ray.direction = get_unit_on_hemi(ray);
+        ray.origin = ray.hit;
     }
 
-
-    imageStore(img, pixelLoc, pixelColor);
+    imageStore(img, PIXEL_LOC, pixel_color);
 }

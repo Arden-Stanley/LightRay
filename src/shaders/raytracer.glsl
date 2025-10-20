6e41b7154082d4fb1c184873f2aca0f5b9f1c4fc
Ardeniscool1293 <arden.stanley@yahoo.com>
2025-10-20 13:28:55 -0500

    Weird visual glitch

--- src/shaders/raytracer.glsl
+++ src/shaders/raytracer.glsl
@@ -34,29 +34,29 @@ struct Viewport {
 };
 
 float get_random_f(vec2 seed) {
-    return fract(sin(dot(seed.xy, vec2(12.9898,78.233))) * 43758.5453);
+    return fract(sin(dot(seed.xy * time, vec2(12.9898,78.233))) * 43758.5453);
 }
 
-vec3 get_random_unit() {
+vec3 get_random_unit(float j) {
     vec3 p;
     float length_squared;
-    float i = 10;
+    int i = 2;
     while (true) {
         p = vec3(
-            (get_random_f(PIXEL_LOC + vec2(41.0 + i, 102.0 + i))), 
-            (get_random_f(PIXEL_LOC + vec2(67.0 + i, 127.0 + i))), 
-            (get_random_f(PIXEL_LOC + vec2(420.0 + i, 273.0 + i)))
+            get_random_f(vec2(float(gl_GlobalInvocationID.x), float(gl_GlobalInvocationID.y))) * 2.0 - 1.0, 
+            get_random_f(vec2(float(gl_GlobalInvocationID.y), float(gl_GlobalInvocationID.y))) * 2.0 - 1.0, 
+            get_random_f(vec2(float(gl_GlobalInvocationID.x), float(gl_GlobalInvocationID.x))) * 2.0 - 1.0
         );
-        i += 10;
+        i += 1;
         length_squared = (p.x * p.x) + (p.y * p.y) + (p.z * p.z);
-        if (length_squared <= 1) {
-            return p / sqrt(length_squared);
+        if (1e-160 < length_squared && length_squared <= 1) {
+            return normalize(p);//  sqrt(length_squared);
         }
     };
 }
 
-vec3 get_unit_on_hemi(Ray ray) {
-    vec3 on_unit_sphere = get_random_unit();
+vec3 get_unit_on_hemi(Ray ray, float j) {
+    vec3 on_unit_sphere = get_random_unit(j);
     if (dot(on_unit_sphere, ray.normal) > 0.0)
         return on_unit_sphere;
     else 
@@ -124,22 +124,23 @@ void main() {
     
     vec3 unit_direction = normalize(ray.direction);
     float a = 0.5 * (unit_direction.y + 1.0);
-    vec3 gradient = (1.0-a) * vec3(1.0, 1.0, 1.0) + a * vec3(0.5, 0.7, 1.0);
+    vec3 gradient = (1.0 - a) * vec3(1.0, 1.0, 1.0) + a * vec3(0.5, 0.7, 1.0);
     vec4 pixel_color = vec4(gradient, 1.0);
     
 
     //vec4 pixel_color = vec4(get_random_unit(), 1.0);
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 4; i++) {
         if (get_sphere_hit(ray, sphere1)) {
-            pixel_color *= vec4(0.5, 0.5, 0.5, 1.0);
+            pixel_color *= vec4(vec3(0.4), 1.0);
         }
         else if (get_sphere_hit(ray, sphere2)) {
-            pixel_color *= vec4(0.5, 0.5, 0.5, 1.0);
+            pixel_color *= vec4(vec3(0.4), 1.0);
         }
         else {
             break;
         }
-        ray.direction = get_unit_on_hemi(ray);
+        ray.direction = get_unit_on_hemi(ray, i + 1);
+        //pixel_color = vec4(ray.direction, 1.0);
         ray.origin = ray.hit;
     }
 
